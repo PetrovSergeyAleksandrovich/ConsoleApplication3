cc2a1ece32cfc7243e9cf79d51cccb58a07e32e3
PetrovSergeyAleksandrovich <ps@dsys.ru>
2020-12-25 12:33:47 +0300

    tmp cmt

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -1,27 +1,9 @@
 #include <iostream>
 
-//Call function std::cout
-void print()
-{
-    //Call operator << for object with type std::cout
-    std::cout << "Hello Skillbox\n";
-}
-
-int print10()
-{
-    std::cout << "10\n";
-    return 10;
-}
-
 
 int main()
 {
-    print();
-    print();
-    print();
-
-    std::cout << print10();
-
+    std::cout << "Hello\n";
     return 0;
 }
 
